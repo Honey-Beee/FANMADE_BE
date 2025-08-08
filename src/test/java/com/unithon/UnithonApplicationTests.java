47638be4eac74a5ea533f1d83dade4b3d80a6794
minjikimkim2222 <olivia0929@naver.com>
2025-08-08 14:39:26 +0900

    springboot 초기 세팅

---
+++ src/test/java/com/unithon/UnithonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unithon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnithonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
